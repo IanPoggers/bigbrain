436873ea678fc10972594c451fc9995b5b9f525c
James Ian Pogue <jamesipogue@protonmail.com>
2021-03-01 22:30:18 -0800

    refactor and add mutate

--- src/main.rs
+++ src/main.rs
@@ -24,6 +24,20 @@ impl Node {
             activation_fn,
         }
     }
+
+    fn random_fn<R: Rng>(rng: &mut R, act_fns: &Vec<(fn(f32) -> f32, f32)>) -> Node {
+        let prob = rng.gen_range(0f32..=act_fns.iter().map(|&(_, x)| x).sum());
+        let mut acc = 0f32;
+        let &(act_fn, _) = act_fns
+            .iter()
+            .skip_while(|&&(_, x)| {
+                acc += x;
+                acc < prob
+            })
+            .next()
+            .unwrap();
+        Node::new(act_fn)
+    }
 }
 
 struct Brain {
@@ -46,6 +60,42 @@ struct RandNetParam {
     outputs: usize,
     min_size: usize,
     max_size: usize,
+}
+
+struct MutateParam {
+    activation_fns: Vec<(fn(f32) -> f32, f32)>,
+    min_node_insertion_percent: f32,
+    max_node_insertion_percent: f32,
+    min_conn_insertion_percent: f32,
+    max_conn_insertion_percent: f32,
+    min_node_removal_percent: f32,
+    max_node_removal_percent: f32,
+    min_conn_removal_percent: f32,
+    max_conn_removal_percent: f32,
+    min_new_stregnth: f32,
+    max_new_stregnth: f32,
+    max_new_node_size: usize,
+    min_new_node_size: usize,
+}
+
+impl MutateParam {
+    fn default(inputs: usize, outputs: usize) -> MutateParam {
+        MutateParam {
+            activation_fns: vec![(|x| x / (1.0 + x.abs()), 1.0)],
+            min_node_insertion_percent: 0.0,
+            max_node_insertion_percent: 0.06,
+            min_conn_insertion_percent: 0.0,
+            max_conn_insertion_percent: 0.06,
+            min_node_removal_percent: 0.0,
+            max_node_removal_percent: 0.06,
+            min_conn_removal_percent: 0.0,
+            max_conn_removal_percent: 0.06,
+            min_new_stregnth: -10.0,
+            max_new_stregnth: 10.0,
+            min_new_node_size: 5,
+            max_new_node_size: 20,
+        }
+    }
 }
 
 impl RandNetParam {
@@ -100,6 +150,61 @@ impl Brain {
             .collect()
     }
 
+    fn mutate(&mut self, param: &MutateParam) {
+        let input_nodes = &self.input_nodes;
+        let hidden_nodes = &mut self.hidden_nodes;
+        let output_nodes = &self.output_nodes;
+        let act_fns = &param.activation_fns;
+        let net = &mut self.net;
+        let mut rng = thread_rng();
+        let node_removals = (rng
+            .gen_range(param.min_node_removal_percent..=param.max_node_removal_percent)
+            * hidden_nodes.len() as f32) as usize;
+
+        // remove random hidden nodes
+        hidden_nodes
+            .iter()
+            .zip(0usize..)
+            .choose_multiple(&mut rng, node_removals);
+
+        // remove random connections
+        let edge_removals = (rng
+            .gen_range(param.min_conn_removal_percent..=param.max_conn_removal_percent)
+            * net.edge_count() as f32) as usize;
+        net.edge_indices()
+            .choose_multiple(&mut rng, edge_removals)
+            .iter()
+            .for_each(|&edge| {
+                net.remove_edge(edge);
+            });
+
+        // @TODO: Make it so this isn't copy-pasted from rand_net
+        // generate new nodes
+        let new_nodes_count = (rng
+            .gen_range(param.min_node_insertion_percent..=param.max_node_insertion_percent)
+            * hidden_nodes.len() as f32) as usize;
+        let mut new_nodes: Vec<NodeIndex> = Vec::new();
+        for _ in 0..new_nodes_count {
+            new_nodes.push(net.add_node(Node::random_fn(&mut rng, &param.activation_fns)));
+        }
+
+        // @TODO: Make it so this isn't copy-pasted from rand_net
+        // connect new nodes
+        for node in new_nodes.iter() {
+            let node_size = rng.gen_range(param.min_new_node_size..=param.max_new_node_size);
+            populate_node(
+                net,
+                input_nodes.iter().chain(hidden_nodes.iter()),
+                &mut rng,
+                node_size,
+                param.min_new_stregnth,
+                param.max_new_stregnth,
+                node,
+            );
+            hidden_nodes.push(*node);
+        }
+    }
+
     fn rand_net(param: &RandNetParam) -> Brain {
         let mut rng = thread_rng();
         let size: usize = rng.gen_range(param.min_size..=param.max_size);
@@ -120,17 +225,7 @@ impl Brain {
 
         // generate nodes
         for _ in 0..size {
-            let prob = rng.gen_range(0f32..=act_fns.iter().map(|&(_, x)| x).sum());
-            let mut acc = 0f32;
-            let &(act_fn, _) = act_fns
-                .iter()
-                .skip_while(|&&(_, x)| {
-                    acc += x;
-                    acc < prob
-                })
-                .next()
-                .unwrap();
-            hidden_nodes.push(net.add_node(Node::new(act_fn)));
+            hidden_nodes.push(net.add_node(Node::random_fn(&mut rng, act_fns)));
         }
 
         // generate output nodes
@@ -139,19 +234,18 @@ impl Brain {
         }
 
         // generate connections
-        for &node in hidden_nodes.iter().chain(output_nodes.iter()) {
+        for node in hidden_nodes.iter().chain(output_nodes.iter()) {
             let node_size = rng.gen_range(param.min_conn_count..=param.max_conn_count);
-            input_nodes
-                .iter()
-                .chain(hidden_nodes.iter())
-                .choose_multiple(&mut rng, node_size)
-                .iter()
-                .for_each(|&&other_node| {
-                    let weight = rng.gen_range(param.min_conn_stregnth..=param.max_conn_stregnth);
-                    net.add_edge(other_node, node, weight);
-                })
+            populate_node(
+                &mut net,
+                input_nodes.iter().chain(hidden_nodes.iter()),
+                &mut rng,
+                node_size,
+                param.min_conn_stregnth,
+                param.max_conn_stregnth,
+                node,
+            );
         }
-
         Brain {
             net,
             input_nodes,
@@ -166,10 +260,57 @@ impl Brain {
 fn main() {
     let mut brain = Brain::rand_net(&RandNetParam::default(5, 3));
     for _ in 0..20 {
-        println!(
-            "{:?}",
-            brain.run(vec![2.1f32, 2.4f32, 3.5f32, 2.4f32, 1.5f32])
-        );
+        println!("{:?}", brain.run(vec![2.1, 2.4, 3.5, 2.4, 1.5]));
+    }
+    brain.mutate(&MutateParam::default(5, 3));
+    for _ in 0..20 {
+        println!("{:?}", brain.run(vec![2.1, 2.4, 3.5, 2.4, 1.5]));
+    }
+}
+
+fn populate_node<'a, R, I>(
+    net: &mut StableGraph<Node, f32>,
+    connected_nodes: I,
+    rng: &mut R,
+    node_size: usize,
+    stregnthmin: f32,
+    stregnthmax: f32,
+    node: &NodeIndex,
+) where
+    R: Rng,
+    I: Iterator<Item = &'a NodeIndex>,
+{
+    connected_nodes
+        .choose_multiple(rng, node_size)
+        .iter()
+        .for_each(|&other_node| {
+            let weight = rng.gen_range(stregnthmin..=stregnthmax);
+            net.add_edge(*other_node, *node, weight);
+        });
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{Brain, MutateParam, RandNetParam};
+
+    #[test]
+    fn simple_test_net() {
+        let mut brain = Brain::rand_net(&RandNetParam::default(5, 3));
+        for _ in 0..20 {
+            println!("{:?}", brain.run(vec![2.1, 2.4, 3.5, 2.4, 1.5]));
+        }
+    }
+
+    #[test]
+    fn test_mutate() {
+        let mut brain = Brain::rand_net(&RandNetParam::default(5, 3));
+        for _ in 0..20 {
+            println!("{:?}", brain.run(vec![2.1, 2.4, 3.5, 2.4, 1.5]));
+        }
+        brain.mutate(&MutateParam::default(5, 3));
+        for _ in 0..20 {
+            println!("{:?}", brain.run(vec![2.1, 2.4, 3.5, 2.4, 1.5]));
+        }
     }
 }
 
